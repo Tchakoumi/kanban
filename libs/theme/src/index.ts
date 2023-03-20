7916e2bd9a2b7a332c332d2fd6605667268e77c1
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-21 00:36:06 +0100

    added modeContext for changing of application theme exported values

--- libs/theme/src/index.ts
+++ libs/theme/src/index.ts
@@ -1,2 +1,5 @@
 export * from './theme';
 export * from './KanbanThemeProvider';
+export * from './modeContext/ModeContext';
+export * from './modeContext/ModeContextProvider';
+export * from './modeContext/mode.interface';
