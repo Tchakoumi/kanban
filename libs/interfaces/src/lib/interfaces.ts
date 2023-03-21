51954e9d795a85b051ccb54d73453b38c46d6b5c
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-22 00:08:17 +0100

    updated interfaces wih respect to design for used case such as creating and editing column with board dialog or creating and editing subtasks with a task dialog

--- libs/interfaces/src/lib/interfaces.ts
+++ libs/interfaces/src/lib/interfaces.ts
@@ -1,13 +1,36 @@
-export interface ICreateBoard {
-  board_name: string;
+export interface ICreateSubtask {
+  subtask_title: string;
 }
 
-export interface IBoard extends ICreateBoard {
-  board_id: string;
+export interface ISubtask extends ICreateSubtask {
+  subtask_id: string;
+  is_done: boolean;
+}
+
+export interface ICreateTask {
+  column_id: string;
+  task_title: string;
+  task_position: number;
+  task_description: string;
+  newSubtasks: ICreateSubtask[];
+}
+
+export interface IEditTask extends Partial<ICreateTask> {
+  deletedTaskIds: string[];
+  updatedSubtasks: Partial<ISubtask>[];
+}
+
+export interface ITask extends Omit<ICreateTask, 'newSubtasks'> {
+  task_id: string;
+  total_done_subtasks: number;
+  total_undone_subtasks: number;
+}
+
+export interface ITaskDetails extends ITask {
+  subtasks: ISubtask[];
 }
 
 export interface ICreateColumn {
-  board_id: string;
   column_title: string;
   column_color_code: string;
 }
@@ -16,25 +39,16 @@ export interface IColumn extends ICreateColumn {
   column_id: string;
 }
 
-export interface ICreateTask {
-  column_id: string;
-  task_title: string;
-  task_position: number;
-  task_description: string;
+export interface ICreateBoard {
+  board_name: string;
+  newColumns: ICreateColumn[];
 }
 
-export interface ITask extends ICreateTask {
-  task_id: string;
-  total_done_subtasks: number;
-  total_undone_subtasks: number;
+export interface IEditBoard extends Partial<ICreateBoard> {
+  deletedColumnIds: string[];
+  updatedColumns: Partial<ICreateColumn>[];
 }
 
-export interface ICreateSubtask {
-  task_id: string;
-  subtask_title: string;
-}
-
-export interface ISubtask extends ICreateSubtask {
-  subtask_id: string;
-  is_done: boolean;
+export interface IBoard extends ICreateBoard {
+  board_id: string;
 }
