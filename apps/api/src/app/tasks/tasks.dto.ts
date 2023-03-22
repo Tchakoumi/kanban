a791d7818165c5f08f1fcb1937b6993d9e13e7e7
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-22 01:25:28 +0100

    created all tasks module dto

---
+++ apps/api/src/app/tasks/tasks.dto.ts
@@ -0,0 +1,71 @@
+import {
+    ICreateSubtask,
+    ICreateTask,
+    IEditTask
+} from '@kanban/interfaces';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+    IsArray,
+    IsBoolean, IsNumber,
+    IsOptional,
+    IsString,
+    IsUUID,
+    ValidateNested
+} from 'class-validator';
+
+export class CreateSubtaskDto implements ICreateSubtask {
+  @IsString()
+  @ApiProperty()
+  subtask_title: string;
+}
+
+export class CreateTaskDto implements ICreateTask {
+  @IsString()
+  @ApiProperty()
+  column_id: string;
+
+  @IsString()
+  @ApiProperty()
+  task_title: string;
+
+  @IsString()
+  @ApiProperty()
+  task_description: string;
+
+  @IsArray()
+  @ApiProperty()
+  @Type(() => CreateSubtaskDto)
+  @ValidateNested({ each: true })
+  newSubtasks: CreateSubtaskDto[];
+}
+
+export class EditSubtaskDto extends PartialType(CreateSubtaskDto) {
+  @IsUUID()
+  @ApiProperty()
+  subtask_id: string;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty()
+  is_done?: boolean;
+}
+
+export class EditTaskDto
+  extends PartialType(CreateTaskDto)
+  implements IEditTask
+{
+  @IsNumber()
+  @ApiProperty()
+  task_position: number;
+
+  @IsString()
+  @ApiProperty()
+  deletedTaskIds: string[];
+
+  @IsArray()
+  @ApiProperty()
+  @Type(() => EditSubtaskDto)
+  @ValidateNested({ each: true })
+  updatedSubtasks: EditSubtaskDto[];
+}
