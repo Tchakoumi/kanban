12ce993777ab8c4f04f46abc85526f76445857c2
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-22 23:55:07 +0100

    extracted layout component
    made it reusable for boards and [board_id] pages.
    
    added the columns component with no colums as this only shows up when there's no board

---
+++ apps/app/components/layout/index.tsx
@@ -0,0 +1,73 @@
+import { IBoard } from '@kanban/interfaces';
+import { generateTheme, useMode } from '@kanban/theme';
+import { Visibility } from '@mui/icons-material';
+import { Box, Button, Tooltip } from '@mui/material';
+import { useState } from 'react';
+import PrimaryNav from '../primaryNav';
+import SecondaryNav from '../secondaryNav';
+
+export default function Layout({
+  children,
+  boards,
+}: {
+  children: JSX.Element | JSX.Element[];
+  boards: IBoard[];
+}) {
+  const { activeMode } = useMode();
+  const [isSecondaryNavOpen, setIsSecondaryNavOpen] = useState<boolean>(true);
+  const theme = generateTheme(activeMode);
+
+  return (
+    <>
+      <Box
+        sx={{
+          height: '100vh',
+          display: 'grid',
+          gridTemplateColumns: 'auto 1fr',
+        }}
+      >
+        <SecondaryNav
+          boards={boards}
+          isSecondaryNavOpen={isSecondaryNavOpen}
+          closeSecondaryNav={() => setIsSecondaryNavOpen(false)}
+        />
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateRows: 'auto 1fr',
+            height: '100%',
+          }}
+        >
+          <PrimaryNav boards={boards} isSecondaryNavOpen={isSecondaryNavOpen} />
+          <Box sx={{ position: 'relative', padding: 3 }}>
+            <Tooltip arrow title="Show Sidebar">
+              <Box
+                component={Button}
+                variant="contained"
+                color="primary"
+                sx={{
+                  position: 'absolute',
+                  left: 0,
+                  bottom: '32px',
+                  backgroundColor: theme.palette.primary.main,
+                  color: theme.common.white,
+                  padding: 1.8125,
+                  borderTopLeftRadius: 0,
+                  borderBottomLeftRadius: 0,
+                  alignItems: 'center',
+                  cursor: 'pointer',
+                  display: isSecondaryNavOpen ? 'none' : 'grid',
+                }}
+                onClick={() => setIsSecondaryNavOpen(true)}
+              >
+                <Visibility />
+              </Box>
+            </Tooltip>
+            {children}
+            {/* <Columns columns={[]} /> */}
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+}
--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -1,14 +1,10 @@
-import { ConfirmDialog } from '@kanban/dialog';
 import { IBoard } from '@kanban/interfaces';
-import { generateTheme, useMode } from '@kanban/theme';
-import { Visibility } from '@mui/icons-material';
-import { Box, Button, Tooltip } from '@mui/material';
 import { GetServerSideProps } from 'next';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import Columns from '../components/columns';
-import PrimaryNav from '../components/primaryNav';
-import SecondaryNav from '../components/secondaryNav';
+import Layout from '../components/layout';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
@@ -35,103 +31,17 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 export function Index({ boards }: { boards: IBoard[] }) {
-  const { activeMode } = useMode();
-  const [isSecondaryNavOpen, setIsSecondaryNavOpen] = useState<boolean>(true);
-  const theme = generateTheme(activeMode);
+  const { push } = useRouter();
 
-  const [activeBoard, setActiveBoard] = useState<IBoard>(
-    boards.length > 0 ? boards[0] : undefined
-  );
-
-  function openBoard(board: IBoard) {
-    setActiveBoard(board);
-  }
-
-  const [isEditBoardDialogOpen, setIsEditBoardDialogOpen] =
-    useState<boolean>(false);
-  const [isConfirmDeleteBoardDialogOpen, setIsConfirmDeleteBoardDialogOpen] =
-    useState<boolean>(false);
-
-  function editBoard() {
-    if (activeBoard) setIsEditBoardDialogOpen(true);
-  }
-
-  function deleteBoard() {
-    if (activeBoard) setIsConfirmDeleteBoardDialogOpen(true);
-  }
+  useEffect(() => {
+    if (boards.length > 0) push(boards[0].board_id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <>
-      <ConfirmDialog
-        closeDialog={() => setIsConfirmDeleteBoardDialogOpen(false)}
-        dialogMessage={`Are you sure you want to delete the "${activeBoard?.board_name}" board? This action will remove all columns and tasks and cannot be reversed.`}
-        isDialogOpen={isConfirmDeleteBoardDialogOpen}
-        confirmButton="Delete"
-        danger
-        dialogTitle="Delete this board?"
-        confirm={() => {
-          alert(`delete activeBoard element`);
-          setActiveBoard(undefined);
-        }}
-      />
-      <Box
-        sx={{
-          height: '100vh',
-          display: 'grid',
-          gridTemplateColumns: 'auto 1fr',
-        }}
-      >
-        <SecondaryNav
-          boards={boards}
-          openBoard={openBoard}
-          activeBoard={activeBoard}
-          isSecondaryNavOpen={isSecondaryNavOpen}
-          closeSecondaryNav={() => setIsSecondaryNavOpen(false)}
-        />
-        <Box
-          sx={{
-            display: 'grid',
-            gridTemplateRows: 'auto 1fr',
-            height: '100%',
-          }}
-        >
-          <PrimaryNav
-            boards={boards}
-            openBoard={openBoard}
-            activeBoard={activeBoard}
-            isSecondaryNavOpen={isSecondaryNavOpen}
-            editBoard={editBoard}
-            deleteBoard={deleteBoard}
-          />
-          <Box sx={{ position: 'relative', padding: 3 }}>
-            <Tooltip arrow title="Show Sidebar">
-              <Box
-                component={Button}
-                variant="contained"
-                color="primary"
-                sx={{
-                  position: 'absolute',
-                  left: 0,
-                  bottom: '32px',
-                  backgroundColor: theme.palette.primary.main,
-                  color: theme.common.white,
-                  padding: 1.8125,
-                  borderTopLeftRadius: 0,
-                  borderBottomLeftRadius: 0,
-                  alignItems: 'center',
-                  cursor: 'pointer',
-                  display: isSecondaryNavOpen ? 'none' : 'grid',
-                }}
-                onClick={() => setIsSecondaryNavOpen(true)}
-              >
-                <Visibility />
-              </Box>
-            </Tooltip>
-            <Columns columns={[]} />
-          </Box>
-        </Box>
-      </Box>
-    </>
+    <Layout boards={boards}>
+      <Columns columns={[]} />
+    </Layout>
   );
 }
 
