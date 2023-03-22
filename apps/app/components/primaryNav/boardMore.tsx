3ff120312ded748da6511fcaf675503eec9a6042
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-22 22:46:47 +0100

    removed delete board props
    brought delete board logic here as deleting a board is no longer global

--- apps/app/components/primaryNav/boardMore.tsx
+++ apps/app/components/primaryNav/boardMore.tsx
@@ -1,27 +1,55 @@
+import { ConfirmDialog } from '@kanban/dialog';
 import { generateTheme } from '@kanban/theme';
 import { MoreVertOutlined } from '@mui/icons-material';
 import { IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
+import { useActiveBoard } from '../../services';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 export default function BoardMore({
   disabled,
   editBoard,
-  deleteBoard,
 }: {
   disabled: boolean;
   editBoard: () => void;
-  deleteBoard: () => void;
 }) {
   const [isMoreMenuOpen, setIsMoreMenuOpen] = useState<boolean>(false);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const {
+    push,
+    query: { board_id },
+  } = useRouter();
+
+  const { activeBoard } = useActiveBoard(board_id as string);
 
   function closeMenu() {
     setIsMoreMenuOpen(false);
     setAnchorEl(null);
   }
 
+  const [isConfirmDeleteBoardDialogOpen, setIsConfirmDeleteBoardDialogOpen] =
+    useState<boolean>(false);
+
+  function deleteBoard() {
+    if (activeBoard) setIsConfirmDeleteBoardDialogOpen(true);
+    else alert('No active board');
+  }
+
   return (
     <>
+      <ConfirmDialog
+        closeDialog={() => setIsConfirmDeleteBoardDialogOpen(false)}
+        dialogMessage={`Are you sure you want to delete the " ${activeBoard?.board_name} " board? This action will remove all columns and tasks and cannot be reversed.`}
+        isDialogOpen={isConfirmDeleteBoardDialogOpen}
+        confirmButton="Delete"
+        danger
+        dialogTitle="Delete this board?"
+        confirm={() => {
+          alert(`delete activeBoard element`);
+          push('/');
+        }}
+      />
+
       <Tooltip arrow title="more">
         <IconButton
           disabled={disabled}
