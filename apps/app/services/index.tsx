9d06e84c75c1a1f4a7ff7ae5beb07eb7eb7bad0d
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-22 22:31:32 +0100

    added a services folder to contain all services
    wrote custom dev service for useActiveBoard

---
+++ apps/app/services/index.tsx
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export function fetcher(url) {
+  return axios.get(url).then((res) => res.data);
+}
+
+export * from './useActiveBoard';
---
+++ apps/app/services/useActiveBoard.ts
@@ -0,0 +1,35 @@
+//TODO: FILE TO WORK IN
+
+import { IBoard } from '@kanban/interfaces';
+import { useState } from 'react';
+
+// import useSWR from 'swr';
+// import {fetcher} from '.';
+
+export function useActiveBoard(board_id: string) {
+  const [data, setData] = useState<{
+    activeBoard: IBoard | undefined;
+    isLoading: boolean;
+    isError: boolean;
+  }>({
+    activeBoard: undefined,
+    isLoading: true,
+    isError: false,
+  });
+  if (board_id === undefined)
+    return {
+      activeBoard: undefined,
+      isLoading: false,
+      isError: false,
+    };
+  //TODO: call api to load activeBoard
+  //   const { data, error, isLoading } = useSWR(`/api/boards/${board_id}`, fetcher);
+  setTimeout(() => {
+    setData({
+      activeBoard: { board_id, board_name: 'Platform Launch' } as IBoard,
+      isLoading: false,
+      isError: false,
+    });
+  }, 3000);
+  return data;
+}
