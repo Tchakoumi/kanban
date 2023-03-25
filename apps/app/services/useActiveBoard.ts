15ef1fb2ffa8d39561199de43c99758c4f5e12ee
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-26 00:07:19 +0100

    added useColumns service

--- apps/app/services/useActiveBoard.ts
+++ apps/app/services/useActiveBoard.ts
@@ -1,6 +1,6 @@
 //TODO: FILE TO WORK IN
 
-import { IBoard } from '@kanban/interfaces';
+import { IBoard, IColumn } from '@kanban/interfaces';
 import { useState } from 'react';
 
 // import useSWR from 'swr';
@@ -29,6 +29,57 @@ export function useActiveBoard(board_id: string) {
       activeBoard: { board_id, board_name: 'Platform Launch' } as IBoard,
       isLoading: false,
       isError: false,
+    });
+  }, 3000);
+  return data;
+}
+
+export function useColumns(board_id: string): {
+  columns: IColumn[];
+  areColumnsLoading: boolean;
+  columnsError: string | undefined;
+} {
+  const [data, setData] = useState<{
+    columns: IColumn[];
+    areColumnsLoading: boolean;
+    columnsError: string | undefined;
+  }>({
+    columns: [],
+    areColumnsLoading: true,
+    columnsError: undefined,
+  });
+
+  if (board_id === undefined)
+    return {
+      columns: [],
+      areColumnsLoading: false,
+      columnsError: 'board_id cannot be undefined.',
+    };
+
+  setTimeout(() => {
+    setData({
+      columns: [
+        {
+          column_color_code: 'lsieos',
+          column_id: 'ieowld',
+          column_position: 2,
+          column_title: 'todo',
+        },
+        {
+          column_color_code: 'lsieos',
+          column_id: 'ieowldo',
+          column_position: 3,
+          column_title: 'Doing',
+        },
+        {
+          column_color_code: 'lsieos',
+          column_id: 'ieowlsdo',
+          column_position: 1,
+          column_title: 'Done',
+        },
+      ],
+      areColumnsLoading: false,
+      columnsError: undefined,
     });
   }, 3000);
   return data;
