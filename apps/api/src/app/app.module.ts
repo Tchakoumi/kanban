ceab487eaf6369a8a6e49fda46347dc9ef9536c4
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-25 18:52:34 +0100

    added encrypt and decrypt interceptors

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { PrismaModule } from '../prisma/prisma.module';
 
 import { AppController } from './app.controller';
+import { AppInterceptor } from './app.interceptor';
+import { AppMiddleware } from './app.middleware';
 import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
 import { ColumnsModule } from './columns/columns.module';
@@ -10,6 +13,16 @@ import { TasksModule } from './tasks/tasks.module';
 @Module({
   imports: [PrismaModule, BoardsModule, ColumnsModule, TasksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AppInterceptor,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AppMiddleware);
+  }
+}
