4b395483c8a7010724fd819c2fe08d9815b216b8
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-27 13:12:54 +0100

    removed columns props and got data using useProps

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -5,33 +5,19 @@ import Columns from '../components/columns';
 import Layout from '../components/layout';
 import { useBoards } from '../services';
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   try {
-//     const boards = await getBoards();
-//     return {
-//       props: {
-//         boards,
-//       },
-//     };
-//   } catch (error) {
-//     toast.error(error.message || 'Oops, There was an error.');
-//     return { notFound: true };
-//   }
-// };
-
 export function Index() {
   const { push } = useRouter();
   const { data: boards, isLoading, error } = useBoards();
 
   useEffect(() => {
-    if (!isLoading && boards.length > 0) push(boards[0].board_id);
+    if (!isLoading && boards && boards.length > 0) push(boards[0].board_id);
     if (error) toast.error(error.message);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoading]);
 
   return (
-    <Layout boards={isLoading ? [] : boards}>
-      <Columns columns={[]} />
+    <Layout>
+      <Columns />
     </Layout>
   );
 }
