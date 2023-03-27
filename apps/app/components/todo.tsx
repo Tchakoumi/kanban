de21e40d9a43952d5e45ef60ea08c372bc2bbcab
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-27 01:33:39 +0100

    added useSubtask hook loading of subtasks on task detail dialog

---
+++ apps/app/components/todo.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+
+export default function Todo() {
+  return (
+    <Box>
+      {/*TODO
+            3. WORK ON DIALOGS
+            4. DISPLAY COLUMNS AND TASKS
+          */}
+    </Box>
+  );
+}
