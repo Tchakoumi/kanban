46873d1f05f89250176e44c682ba4997dce41e90
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-27 01:34:42 +0100

    added create task dialog task on create task click

--- apps/app/components/primaryNav/index.tsx
+++ apps/app/components/primaryNav/index.tsx
@@ -1,16 +1,18 @@
-import { IBoard } from '@kanban/interfaces';
+import { IBoard, ICreateTask } from '@kanban/interfaces';
 import { generateTheme, useMode } from '@kanban/theme';
+import { ErrorMessage, useNotification } from '@kanban/toast';
 import { AddOutlined, ReportRounded } from '@mui/icons-material';
 import { Box, Button, Divider, Tooltip, Typography } from '@mui/material';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import favicon from '../../public/favicon_colored.png';
 import logo_dark from '../../public/logo_dark.png';
 import logo_light from '../../public/logo_light.png';
+import { useActiveBoard, useColumns } from '../../services';
+import ManageTaskDialog from '../task/manageTaskDialog';
 import ActiveBoard from './activeBoard';
 import BoardMore from './boardMore';
-import { useRouter } from 'next/router';
-import { useActiveBoard, useColumns } from '../../services';
-import { useNotification } from '@kanban/toast';
 
 export default function PrimaryNav({
   isSecondaryNavOpen,
@@ -41,95 +43,102 @@ export default function PrimaryNav({
     });
   }
 
+  const [isAddDialogOpen, setIsAddDialogOpen] = useState<boolean>(false);
+
+  const [submissionNotif, setSubmissionNotif] = useState<useNotification>();
+
+  function createTask(task: ICreateTask) {
+    const notif = new useNotification();
+    if (submissionNotif) {
+      submissionNotif.dismiss();
+    }
+    setSubmissionNotif(notif);
+    notif.notify({
+      render: 'Creating task...',
+    });
+    setTimeout(() => {
+      //TODO: CALL API HERE TO create task with data task
+      // eslint-disable-next-line no-constant-condition
+      if (5 > 4) {
+        notif.update({
+          render: 'Task Created',
+          autoClose: 2000,
+        });
+        setSubmissionNotif(undefined);
+      } else {
+        notif.update({
+          type: 'ERROR',
+          render: (
+            <ErrorMessage
+              retryFunction={() => createTask(task)}
+              notification={notif}
+              //TODO: message should come from backend
+              message={
+                'Something went wrong while creating task. Please try again!!!'
+              }
+            />
+          ),
+          autoClose: 2000,
+          icon: () => <ReportRounded fontSize="medium" color="error" />,
+        });
+      }
+    }, 3000);
+
+    //TODO: MUTATE useBoardDetails
+  }
+
   return (
-    <Box
-      sx={{
-        display: 'grid',
-        gridTemplateColumns: {
-          mobile: 'auto 1fr',
-          tablet: isSecondaryNavOpen ? '1fr' : 'auto 1fr',
-        },
-        columnGap: 3,
-        backgroundColor:
-          activeMode === 'light' ? theme.common.white : theme.common.dark_grey,
-        alignItems: 'center',
-        paddingLeft: 3,
-        paddingRight: 3,
-        height: {
-          desktop: '96px',
-          tabled: '80px',
-          mobile: '64px',
-        },
-      }}
-    >
-      <Box
-        sx={{
-          display: {
-            mobile: 'grid',
-            tablet: isSecondaryNavOpen ? 'none' : 'grid',
-          },
-          alignItems: 'center',
-        }}
-      >
-        <Box
-          sx={{
-            display: {
-              mobile: 'none',
-              tablet: 'block',
-            },
-          }}
-        >
-          <Image
-            src={activeMode === 'dark' ? logo_dark : logo_light}
-            alt="Kanban"
-          />
-        </Box>
-        <Box
-          sx={{
-            display: {
-              mobile: 'block',
-              tablet: 'none',
-            },
-          }}
-        >
-          <Image src={favicon} alt="Kanban" />
-        </Box>
-      </Box>
+    <>
+      <ManageTaskDialog
+        closeDialog={() => setIsAddDialogOpen(false)}
+        isDialogOpen={isAddDialogOpen}
+        submitDialog={createTask}
+      />
       <Box
         sx={{
           display: 'grid',
-          gridTemplateColumns: isSecondaryNavOpen
-            ? '1fr auto'
-            : 'auto 1fr auto',
+          gridTemplateColumns: {
+            mobile: 'auto 1fr',
+            tablet: isSecondaryNavOpen ? '1fr' : 'auto 1fr',
+          },
           columnGap: 3,
-          alignSelf: 'stretch',
+          backgroundColor:
+            activeMode === 'light'
+              ? theme.common.white
+              : theme.common.dark_grey,
           alignItems: 'center',
+          paddingLeft: 3,
+          paddingRight: 3,
+          height: {
+            desktop: '96px',
+            tabled: '80px',
+            mobile: '64px',
+          },
         }}
       >
-        <Divider
-          sx={{
-            display: isSecondaryNavOpen ? 'none' : 'block',
-            backgroundColor:
-              generateTheme(activeMode).common[
-                activeMode === 'light' ? 'line_light' : 'line_dark'
-              ],
-            alignSelf: 'stretch',
-          }}
-          orientation="vertical"
-        />
-        <ActiveBoard boards={boards} />
         <Box
           sx={{
-            justifySelf: 'end',
-            display: 'grid',
+            display: {
+              mobile: 'grid',
+              tablet: isSecondaryNavOpen ? 'none' : 'grid',
+            },
             alignItems: 'center',
-            gridAutoFlow: 'column',
-            gap: 1,
           }}
         >
-          <Tooltip
-            arrow
-            title="Add new task"
+          <Box
+            sx={{
+              display: {
+                mobile: 'none',
+                tablet: 'block',
+              },
+            }}
+          >
+            <Image
+              src={activeMode === 'dark' ? logo_dark : logo_light}
+              alt="Kanban"
+            />
+          </Box>
+          <Box
             sx={{
               display: {
                 mobile: 'block',
@@ -137,44 +146,91 @@ export default function PrimaryNav({
               },
             }}
           >
-            <Button
-              color="primary"
-              variant="contained"
-              disabled={areColumnsLoading || columns.length === 0}
-              startIcon={<AddOutlined />}
+            <Image src={favicon} alt="Kanban" />
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: isSecondaryNavOpen
+              ? '1fr auto'
+              : 'auto 1fr auto',
+            columnGap: 3,
+            alignSelf: 'stretch',
+            alignItems: 'center',
+          }}
+        >
+          <Divider
+            sx={{
+              display: isSecondaryNavOpen ? 'none' : 'block',
+              backgroundColor:
+                generateTheme(activeMode).common[
+                  activeMode === 'light' ? 'line_light' : 'line_dark'
+                ],
+              alignSelf: 'stretch',
+            }}
+            orientation="vertical"
+          />
+          <ActiveBoard boards={boards} />
+          <Box
+            sx={{
+              justifySelf: 'end',
+              display: 'grid',
+              alignItems: 'center',
+              gridAutoFlow: 'column',
+              gap: 1,
+            }}
+          >
+            <Tooltip
+              arrow
+              title="Add new task"
               sx={{
-                '&:disabled': {
-                  backgroundColor: 'rgba(99, 95, 199, 1)',
-                },
-                '& .MuiButton-startIcon': {
-                  marginRight: {
-                    mobile: 0,
-                    tablet: 1,
-                  },
-                  marginLeft: {
-                    mobile: 0,
-                    tablet: -0.5,
-                  },
+                display: {
+                  mobile: 'block',
+                  tablet: 'none',
                 },
               }}
             >
-              <Typography
-                variant="button"
+              <Button
+                color="primary"
+                variant="contained"
+                disabled={areColumnsLoading || columns.length === 0}
+                startIcon={<AddOutlined />}
+                onClick={() => setIsAddDialogOpen(true)}
                 sx={{
-                  textTransform: 'none',
-                  display: {
-                    tablet: 'block',
-                    mobile: 'none',
+                  '&:disabled': {
+                    backgroundColor: 'rgba(99, 95, 199, 1)',
+                  },
+                  '& .MuiButton-startIcon': {
+                    marginRight: {
+                      mobile: 0,
+                      tablet: 1,
+                    },
+                    marginLeft: {
+                      mobile: 0,
+                      tablet: -0.5,
+                    },
                   },
                 }}
               >
-                Add New Task
-              </Typography>
-            </Button>
-          </Tooltip>
-          <BoardMore disabled={!activeBoard} />
+                <Typography
+                  variant="button"
+                  sx={{
+                    textTransform: 'none',
+                    display: {
+                      tablet: 'block',
+                      mobile: 'none',
+                    },
+                  }}
+                >
+                  Add New Task
+                </Typography>
+              </Button>
+            </Tooltip>
+            <BoardMore disabled={!activeBoard} />
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 }
