bdf7d377693e9309f3d84be677f4f2b2d7d0e9cc
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-31 18:30:52 +0100

    added newColumn dialog

---
+++ apps/app/components/columns/newColumnDialog.tsx
@@ -0,0 +1,97 @@
+import { DialogTransition } from '@kanban/dialog';
+import { ICreateColumn } from '@kanban/interfaces';
+import { generateTheme, useMode } from '@kanban/theme';
+import { Box, Button, Dialog, TextField, Typography } from '@mui/material';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+export default function ManageTaskDialog({
+  isDialogOpen,
+  closeDialog,
+  submitDialog,
+}: {
+  isDialogOpen: boolean;
+  closeDialog: () => void;
+  submitDialog: (val: ICreateColumn) => void;
+}) {
+  const { activeMode } = useMode();
+  const theme = generateTheme(activeMode);
+
+  const initialValues: ICreateColumn = {
+    column_color_code: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+    column_title: '',
+  };
+
+  const validationSchema = Yup.object().shape({
+    column_title: Yup.string().required(),
+  });
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    enableReinitialize: true,
+    onSubmit: (values, { resetForm }) => {
+      submitDialog(values);
+      resetForm();
+      handleClose();
+    },
+  });
+
+  function handleClose() {
+    closeDialog();
+    formik.resetForm();
+  }
+
+  return (
+    <Dialog
+      open={isDialogOpen}
+      TransitionComponent={DialogTransition}
+      onClose={handleClose}
+    >
+      <Box
+        sx={{
+          backgroundColor:
+            theme.palette.mode === 'dark' ? theme.common.dark_grey : '',
+          width: {
+            mobile: 'initial',
+            tablet: '480px',
+          },
+          padding: '32px',
+          display: 'grid',
+          rowGap: 3,
+        }}
+      >
+        <Typography variant="h2">Add New Column</Typography>
+        <Box
+          sx={{ display: 'grid', rowGap: 3 }}
+          component="form"
+          onSubmit={formik.handleSubmit}
+        >
+          <TextField
+            fullWidth
+            required
+            autoFocus
+            label="Title"
+            size="small"
+            error={
+              formik.touched.column_title && Boolean(formik.errors.column_title)
+            }
+            helperText={
+              formik.touched.column_title && formik.errors.column_title
+            }
+            {...formik.getFieldProps('.column_title')}
+          />
+
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            disableElevation
+          >
+            Create Column
+          </Button>
+        </Box>
+      </Box>
+    </Dialog>
+  );
+}
