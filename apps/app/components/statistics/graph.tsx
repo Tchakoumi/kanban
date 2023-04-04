6c5a3d8fc97650fcf5f8760379095f92c0712c4c
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-04-04 13:04:08 +0100

    added statis display component

---
+++ apps/app/components/statistics/graph.tsx
@@ -0,0 +1,167 @@
+import { generateTheme, useMode } from '@kanban/theme';
+import { useNotification } from '@kanban/toast';
+import { ReportRounded } from '@mui/icons-material';
+import { Box, Skeleton } from '@mui/material';
+import Chart, { ChartItem } from 'chart.js/auto';
+import { useEffect } from 'react';
+import { useIntl } from 'react-intl';
+import { useStatistics } from '../../services';
+
+export default function Graph() {
+  const { formatNumber, formatDate } = useIntl();
+  const { activeMode } = useMode();
+  const theme = generateTheme(activeMode);
+
+  const {
+    data,
+    error: graphDataError,
+    isLoading: isDataLoading,
+  } = useStatistics();
+
+  useEffect(() => {
+    if (graphDataError) {
+      const notif = new useNotification();
+      notif.notify({ render: 'Notifying' });
+      notif.update({
+        type: 'ERROR',
+        render:
+          graphDataError?.message ??
+          'Something went wrong while loading boards ',
+        autoClose: 3000,
+        icon: () => <ReportRounded fontSize="medium" color="error" />,
+      });
+    }
+  }, [graphDataError]);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const config = (data: any) => {
+    return {
+      type: 'line',
+      data,
+      options: {
+        maintainAspectRatio: true,
+        responsive: true,
+        // aspectRatio: ,
+        tension: 0.2,
+        scales: {
+          y: {
+            stacked: true,
+            grid: {
+              display: true,
+              color: theme.common.line_light,
+            },
+          },
+          x: {
+            grid: {
+              display: true,
+              color: theme.common.line_light,
+            },
+          },
+        },
+        parsing: {
+          xAxisKey: 'datetime',
+          yAxisKey: 'count',
+        },
+      },
+    };
+  };
+
+  const updateGraph = () => {
+    const dataChart = Chart.getChart('dataChart');
+    if (dataChart) dataChart.destroy();
+
+    new Chart(
+      document.getElementById('dataChart') as ChartItem,
+      config({
+        datasets: [
+          {
+            label: 'Task movements over time',
+            data: (data
+              ? [
+                  { count: 0, datetime: new Date('04/03/2023') },
+                  ...data.movedTasksStats,
+                ]
+              : []
+            )
+              .sort((a, b) => (a.datetime > b.datetime ? -1 : 1))
+              .map(({ datetime, count }) => ({
+                count: formatNumber(count),
+                datetime: formatDate(datetime, {
+                  year: '2-digit',
+                  month: 'short',
+                  day: '2-digit',
+                }),
+              })),
+            borderColor: theme.palette.primary.light,
+            fill: true,
+            order: 2,
+          },
+          {
+            label: 'Task updates over time',
+
+            data: (data
+              ? [
+                  { count: 0, datetime: new Date('04/03/2023') },
+                  ...data.updatedTaskStats,
+                ]
+              : []
+            )
+              .sort((a, b) => (a.datetime > b.datetime ? -1 : 1))
+              .map(({ datetime, count }) => ({
+                count: formatNumber(count),
+                datetime: formatDate(datetime, {
+                  year: '2-digit',
+                  month: 'short',
+                  day: '2-digit',
+                }),
+              })),
+
+            borderColor: theme.palette.error.light,
+            fill: true,
+            order: 2,
+          },
+        ],
+      })
+    );
+  };
+
+  useEffect(() => {
+    updateGraph();
+    // eslint-disable-next-line
+  }, [data]);
+
+  return (
+    <>
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: isDataLoading ? 'none' : 'grid',
+          justifyItems: 'center',
+        }}
+      >
+        <Box component={'canvas'} id="dataChart"></Box>
+      </div>
+      {isDataLoading && (
+        <div
+          style={{
+            display: 'grid',
+            gridAutoFlow: 'column',
+            columnGap: theme.spacing(1),
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          {[...new Array(9)].map((_, index) => (
+            <Skeleton
+              key={index}
+              variant="rectangular"
+              animation="wave"
+              height={`${(index + 1) * 10}%`}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
---
+++ apps/app/pages/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '../../components/layout';
+import Graph from '../../components/statistics/graph';
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <Graph />
+    </Layout>
+  );
+}
