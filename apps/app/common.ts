7865dd87e4946df2624eda50880796710995c8c4
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-04-05 12:38:52 +0100

    updated random color generator

--- apps/app/common.ts
+++ apps/app/common.ts
@@ -1,3 +1,3 @@
 export default function randomColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return '#000000'.replace(/0/g, () => (~~(Math.random() * 16)).toString(16));
 }
